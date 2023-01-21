02b9fa52c3c5ed0370b992ad32f8fbb7285aae8a
h3llomoto <alexey.k.here@yandex.ru>
2023-01-21 15:46:13 +0300

    fixes

--- dev/serve.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue';
-import Dev from './serve.vue';
-import qyasarOptions from './quasar-user-options';
-import { Quasar } from 'quasar';
-
-const app = createApp(Dev);
-app.use(Quasar, qyasarOptions);
-app.mount('#app');
--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const extendConf = function (conf) {
   console.log(` App Extension (QPhoneInput) Info: 'Adding qzoom boot reference to your quasar.conf.js'`)
 
   // make sure boot & component files transpile
-  conf.build.transpileDependencies.push(/quasar-app-extension-qzoom[\\/]src/)
+  conf.build.transpileDependencies.push(/quasar-app-extension-qphoneinput[\\/]src/)
 
   // make sure these plugins are in the build
   // conf.framework.plugins.push('AppFullscreen')
@@ -21,8 +21,8 @@ const extendConf = function (conf) {
   // conf.framework.directives.push('Scroll')
 
   // make sure qzoom css goes through webpack to avoid ssr issues
-  conf.css.push('~@quasar/quasar-app-extension-qzoom/src/component/zoom.styl')
-  console.log(` App Extension (qzoom) Info: 'Adding zoom.styl css reference to your quasar.conf.js'`)
+  conf.css.push('~@quasar/quasar-app-extension-qzoom/src/style.scss')
+  console.log(` App Extension (QPhoneInput) Info: 'Adding style.scss css reference to your quasar.conf.js'`)
 }
 
 module.exports = function (api) {
